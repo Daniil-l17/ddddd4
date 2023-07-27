bd13516a4f89eb1bcb4211ead0f76acbe440d55c
Daniil-l17 <129774580+Daniil-l17@users.noreply.github.com>
2023-07-27 20:23:07 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import  Navbar from './componends/navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/componends/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './style.scss'
+
+const Navbar = () => {
+  return <>
+  <header>
+    <h1>ssdsdsdssdsss</h1>
+    <p>привет меня зовут Даниил</p>
+  </header>
+  </>
+}
+
+
+
+  export default Navbar;
+
